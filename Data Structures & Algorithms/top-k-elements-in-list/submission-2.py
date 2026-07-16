851d9c2e84a4fc6cf1c21c7d1254205c8acf8cbd
janeturi <jurios2@emory.edu>
2026-07-15 22:00:29 -0400

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+    # given int array nums and int k, return the k most
+    # freq elemenets within the array
+    
+    # create freq map to keep track of frequencies (if seen b4, count++)
+    # -> now we need to actually find a way to sort the frequencies to return max
+    # 
+
+        count = {}
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+
+        arr = []
+        for num, cnt in count.items():
+            arr.append([cnt, num])
+        arr.sort()
+
+        res = []
+        for i in range(1, k + 1):
+            res.append(arr[-i][1])
+
+        return res
